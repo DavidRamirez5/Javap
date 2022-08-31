e4ae3451c40a7114d326da96ea8dc9d0863cb396
David Patiño <david.andrespr5@gmail.com>
2022-08-31 15:14:38 -0500

    Tipos de datos

--- intro/src/com.intro/App.java
+++ intro/src/com.intro/App.java
@@ -1,5 +1,29 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Tipos de datos en JAVA.
+        // Enteros
+        byte value1 = 10;
+        System.out.println(value1);
+        short value2 = 15;
+        System.out.println(value2);
+        int value3 = 30;
+        System.out.println(value3);
+        long value4 =100;
+        System.out.println(value4);
+        // Decimales
+        float value5 = 5.5f;
+        System.out.println(value5);
+        double value6 = 10.9d;
+        System.out.println(value6);
+        // Booleanos
+        boolean value7 = true;
+        System.out.println(value7);
+        boolean value8 = false;
+        System.out.println(value8);
+        //texto
+        char value9 = 'a';
+        System.out.println(value9);
+        String value10 ="Hola";
+        System.out.println(value10);
     }
 }
